e3fbb93e0dd789e71c03a8668ca837752ae5a830
MerkushevKirill <lanwen@yandex-team.ru>
2014-10-12 01:35:28 +0400

    fix - return status on multi-update events

--- application-module/src/main/java/ru/sharmana/resources/EventResource.java
+++ application-module/src/main/java/ru/sharmana/resources/EventResource.java
@@ -1,10 +1,16 @@
 package ru.sharmana.resources;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableListMultimap;
 import jersey.repackaged.com.google.common.base.Preconditions;
 import org.eclipse.jetty.http.HttpStatus;
 import org.jongo.MongoCollection;
 import org.jongo.MongoCursor;
 import ru.sharmana.beans.Event;
+import ru.sharmana.beans.Status;
 import ru.sharmana.beans.Transaction;
 import ru.sharmana.beans.User;
 import ru.sharmana.misc.DBActions;
@@ -19,18 +25,20 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.collect.FluentIterable.from;
 import static jersey.repackaged.com.google.common.collect.Lists.newArrayList;
 import static ru.sharmana.misc.DataActions.mergeTransactions;
 
-@Path("event")
+@Path("")
 public class EventResource {
 
     public static final String EVENTS_COLLECTION = "events";
 
     @PUT
-    @Path("add")
+    @Path("event/add")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response addEvent(Event event) throws IOException {
@@ -46,6 +54,7 @@ public class EventResource {
 
             List<Transaction> merged = mergeTransactions(writed.getTransactions(), event.getTransactions());
             writed.setTransactions(merged);
+
             dbEvents.save(writed);
             return Response.ok(writed).build();
         }
@@ -54,8 +63,70 @@ public class EventResource {
         return Response.status(HttpStatus.CREATED_201).entity(event).build();
     }
 
+    @PUT
+    @Path("events/add")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addEvents(List<Event> events) throws IOException {
+        Preconditions.checkNotNull(events);
+
+        MongoCollection dbEvents = DBActions.getCollection(EVENTS_COLLECTION);
+
+        final ImmutableListMultimap<String, Event> ids = from(events)
+                .filter(hasId())
+                .index(extractId());
+
+        MongoCursor<Event> writed = dbEvents.find("{_id:{$in:#}}", ids.keys()).as(Event.class);
+
+        List<Event> toMerge = new ArrayList<>();
+
+        for (Event exists : writed) {
+             exists.setTransactions(mergeTransactions(exists.getTransactions(),
+                     ids.get(exists.getId()).get(0).getTransactions()));
+            toMerge.add(exists);
+        }
+
+        dbEvents.save(toMerge);
+
+        final ImmutableListMultimap<String, Event> idsMerged = from(toMerge)
+                .index(extractId());
+
+        ImmutableList<Event> newEvents = from(events).filter(Predicates.not(hasId()))
+                .append(from(events).filter(new Predicate<Event>() {
+            @Override
+            public boolean apply(Event input) {
+                return !idsMerged.containsKey(input.getId());
+            }
+        })).toList();
+
+
+        dbEvents.insert(newEvents);
+
+        return Response.status(HttpStatus.CREATED_201)
+                .entity(new Status().withCreated((long)newEvents.size())
+                        .withUpdated((long)toMerge.size())).build();
+    }
+
+    private Function<Event, String> extractId() {
+        return new Function<Event, String>() {
+            @Override
+            public String apply(Event input) {
+                return input.getId();
+            }
+        };
+    }
+
+    private Predicate<Event> hasId() {
+        return new Predicate<Event>() {
+            @Override
+            public boolean apply(Event input) {
+                return input.getId() != null;
+            }
+        };
+    }
+
     @GET
-    @Path("my")
+    @Path("events/my")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Event> myEvents(@HeaderParam(HttpHeaders.AUTHORIZATION) String token) {
         MongoCollection events = DBActions.getCollection(EVENTS_COLLECTION);
